25e51e915df872fcb0d9868e2b943cbea2c3975c
Shiyu Cheng <starkeyshiyu@gmail.com>
2019-09-14 21:43:31 -0700

    configured nev items layout@animation

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,10 @@
 const menuBtn = document.querySelector(".menu-btn");
-const hanmburger = document.querySelector(".menu-btn__burger");
+const hamburger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
+
+
 
 let showMenu = false;
 
@@ -8,11 +13,25 @@ menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu(){
     if(!showMenu){
-        hanmburger.classList.add("open");
+        hamburger.classList.add("open");
+        nav.classList.add("open");
+        menuNav.classList.add("open");
+        navItems.forEach(item => {
+            item.classList.add("open");
+        });
+
         showMenu = true;
     }else {
-        hanmburger.classList.remove("open");
+        hamburger.classList.remove("open");
+        nav.classList.remove("open");
+        menuNav.classList.remove("open");
+        navItems.forEach(item => {
+            item.classList.remove("open");
+        });
+
         showMenu = false;
     }
-};
+}
+
+
 
